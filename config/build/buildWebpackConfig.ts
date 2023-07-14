073a01367ce01cf2ec65b3aba29f7fe470429e5a
olegweremey <olegweremey1994@mail.ru>
2023-07-14 23:57:41 +0300

    Initial commit

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,27 @@
+import {BuildOptions} from "./types/config";
+import webpack from "webpack";
+import {buildPlugin} from "./buildPlugin";
+import {buildLoaders} from "./buildLoaders";
+import {buildResolves} from "./buildResolves";
+import {buildDevServer} from "./buildDevServer";
+
+export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+
+  const {mode, paths, isDev} = options
+  return {
+    mode,
+    entry: paths.entry,
+    output: {
+      filename: "[name].[contenthash].js",
+      path: paths.build,
+      clean: true
+    },
+    plugins: buildPlugin(options),
+    module: {
+      rules: buildLoaders()
+    },
+    resolve: buildResolves(),
+    devtool: isDev ? 'inline-source-map' : undefined,
+    devServer: isDev ? buildDevServer(options) : undefined,
+  }
+}
